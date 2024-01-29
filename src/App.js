e14c87108c46172938d36546c4e3fdd3c90217a8
ENNA-Egor <e.yudakov@gmail.com>
2024-01-29 14:08:57 +0300

    собрал Todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from "react-redux";
+import { addTodo, removeTodo, toggleTodo } from "./store";
 
-function App() {
+import "./styles.css";
+
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello Redux Todo</h1>
+      <NewTodo />
+      <TodoList />
     </div>
   );
 }
 
-export default App;
+const NewTodo = () => {
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addTodo(event.target.title.value));
+    event.target.reset();
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input type="text" name="title" placeholder="new todo" />
+      <input type="submit" value="Add Todo" />
+    </form>
+  );
+};
+
+const TodoList = () => {
+  const todos = useSelector((state) => state);
+  const dispatch = useDispatch();
+
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li key={todo.title}>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => dispatch(toggleTodo(todo.id))}
+          />{" "}
+          {todo.title}{" "}
+          <button onClick={() => dispatch(removeTodo(todo.id))}>delete</button>
+        </li>
+      ))}
+    </ul>
+  );
+};
+
