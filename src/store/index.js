86e3e8fda651601a30ea763f6c3a66a694821a56
ENNA-Egor <e.yudakov@gmail.com>
2024-02-01 09:04:09 +0300

    Надо разобраться с configureStore

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,13 @@
 import {createStore} from 'redux';
 import {rootReduser} from './redusers';
+import {loadState} from './helper/local-storage';
 
-export const store = createStore(rootReduser, [], window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+// export const configureStore = () => {
+    const persistedState = loadState();
+
+    export const store = createStore(rootReduser, persistedState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+//     return store;
+// }
+
